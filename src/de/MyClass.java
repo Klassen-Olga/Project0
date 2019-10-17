1aaf63199ae7ed5109584d74bca615db1242e0b6
Klassen.Olga <lefelog@mail.ru>
2019-10-17 20:01:35 +0200

    i am master

--- src/de/MyClass.java
+++ src/de/MyClass.java
@@ -2,6 +2,7 @@ package de;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World! You are so beautiful! I'm happy");
+        System.out.println("Hello World! You are so beautiful! I'm happy" +
+                "I'm master!");
     }
 }
