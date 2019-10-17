8bfda0b9b1ed7316a3d2c8fa68c6d3ad67e6adae
Klassen <lefelog@mail.ru>
2019-10-17 20:04:42 +0200

    this is del notebook

--- src/de/MyClass.java
+++ src/de/MyClass.java
@@ -3,6 +3,7 @@ package de;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World! You are so beautiful! I'm happy" +
-                "I'm master!");
+                "I'm master!" +
+                "This is dell notebook");
     }
 }
