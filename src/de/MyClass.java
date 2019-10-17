bdcecab22a6c14091a07e018482690d92583c224
Klassen.Olga <lefelog@mail.ru>
2019-10-17 19:40:37 +0200

    u are so beautiful

--- src/de/MyClass.java
+++ src/de/MyClass.java
@@ -2,6 +2,6 @@ package de;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! You are so beautiful");
     }
 }
